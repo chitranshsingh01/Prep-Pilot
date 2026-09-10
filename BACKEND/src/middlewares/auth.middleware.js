b26020b00fba7c58b2733ecd015bc7b85c7fe3aa
Chitransh Singh <chitranshsingh02@gmail.com>
2026-09-10 15:26:41 +0530

    Add frontend and organize backend

--- BACKEND/src/middlewares/auth.middleware.js
+++ BACKEND/src/middlewares/auth.middleware.js
@@ -1,10 +1,20 @@
 const jwt = require('jsonwebtoken');
+const tokenblacklistModel=require('../models/blacklist.model');
 
-function authUser(req,res,next){
+async function authUser(req,res,next){
     const token=req.cookies.token;
+
     if(!token){
         return res.status(401).json({message:"Unauthorized"});
     }
+    
+     const istokenBlacklisted= await tokenblacklistModel.findOne({token});
+
+     if(istokenBlacklisted){
+       return res.status(401).json({message:"Token is blacklisted,please login first"});
+     }
+     
+
     try{
         const decoded =jwt.verify(token,process.env.JWT_SECRET_KEY);
         req.user=decoded;
