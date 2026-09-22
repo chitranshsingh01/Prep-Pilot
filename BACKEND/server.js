4c4875549e7eeb1674a765be62033bbf93191a58
Chitransh Singh <chitranshsingh02@gmail.com>
2026-09-22 15:41:27 +0530

    port update

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -11,8 +11,8 @@ dns.setServers([
 ]);
 
 connectDB();
+const PORT = process.env.PORT || 3000;
 
-
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('Server is running on port 3000');
 });
